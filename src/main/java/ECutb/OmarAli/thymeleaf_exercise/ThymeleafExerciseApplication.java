794701ed6b3078ef6cbbf6e58941f8da001ff712
Omar Ali <omarabdiali0@gmail.com>
2020-04-19 17:11:26 +0200

    Completed thymeleaf

---
+++ src/main/java/ECutb/OmarAli/thymeleaf_exercise/ThymeleafExerciseApplication.java
@@ -0,0 +1,13 @@
+package ECutb.OmarAli.thymeleaf_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ECutb/OmarAli/thymeleaf_exercise/ThymeleafExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package ECutb.OmarAli.thymeleaf_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
